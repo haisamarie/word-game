cde6e5e43bb1cc22f779b4eac2798dc898d87d02
haisamarie <marie.199204@gmail.com>
2025-11-18 12:41:37 +0900

    feat: updateRevealedState関数の追加

--- _tests/game.test.js
+++ _tests/game.test.js
@@ -1,7 +1,34 @@
-import { createHiddenState } from "../src/game";
+import { createHiddenState,updateRevealedState } from "../src/game";
 
 describe("createHiddenState", () => {
  test("同じ文字数の'_'配列を返す", () => {
     expect(createHiddenState("apple")).toEqual(["_", "_", "_", "_", "_"]);
   });
+});
+
+describe("updateRevealedState", () => {
+  test("一致する文字のみ置き換える", () => {
+    const result = updateRevealedState("apple", ["_", "_", "_", "_", "_"], "p");
+    expect(result).toEqual(["_", "p", "p", "_", "_"]);
+  });
+
+  test("一致しない場合は変化しない", () => {
+    const result = updateRevealedState("apple", ["_", "_", "_", "_", "_"], "x");
+    expect(result).toEqual(["_", "_", "_", "_", "_"]);
+  });
+
+  test("大文字小文字は区別しない", () => {
+    const result = updateRevealedState("APPLE", ["_", "_", "_", "_", "_"], "p");
+    expect(result).toEqual(["_", "p", "p", "_", "_"]);
+  });
+
+  test("単語内の同じ文字は全て置き換わる", () => {
+    const word = "banana";
+    const currentState = ["_", "_", "_", "_", "_", "_"];
+    const guessChar = "a";
+
+    const result = updateRevealedState(word, currentState, guessChar);
+    expect(result).toEqual(["_", "a", "_", "a", "_", "a"]);
+  });
+
 });
--- src/game.js
+++ src/game.js
@@ -2,3 +2,10 @@
 export const createHiddenState = (word) => {
   return Array.from(word, () => "_");
 }
+
+export const updateRevealedState = (word, currentState, guessChar) => {
+  const guess = guessChar.toLowerCase();
+  const chars = Array.from(word.toLowerCase());
+
+  return currentState.map((c, i) => (chars[i] === guess ? guess : c));
+}
